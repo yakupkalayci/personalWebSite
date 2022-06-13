1e467a5469183c61bb1d59b34f96737f69be707f
yakupkalayci <ykpklyc18@gmail.com>
2022-06-13 15:49:27 +0300

    make last changes

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const mainTitle = document.querySelector(".mainTitle > a");
 const changeTheme = () => {
     if(document.body.classList.contains("darkMode")) {
         themeButton.textContent = "Dark";
-        themeButton.style.backgroundColor = "rgb(0, 47, 100)";
-        themeButton.style.color = "rgb(238, 246, 255)";
+        themeButton.classList.add("darkMode");
+        themeButton.classList.remove("lightMode");
+        // themeButton.style.backgroundColor = "rgb(0, 47, 100)";
+        // themeButton.style.color = "rgb(238, 246, 255)";
         document.body.classList.remove("darkMode");
         header.style.borderBottomColor = "rgb(0, 47, 100)";
         mainTitle.style.color = "rgb(0, 47, 100)";
     } else {
         themeButton.textContent = "Light";
-        themeButton.style.backgroundColor = "rgb(238, 246, 255)";
-        themeButton.style.color = "rgb(0, 47, 100)";
+        themeButton.classList.remove("darkMode");
+        themeButton.classList.add("lightMode");
+        // themeButton.style.backgroundColor = "rgb(238, 246, 255)";
+        // themeButton.style.color = "rgb(0, 47, 100)";
         document.body.classList.add("darkMode");
         header.style.borderBottomColor = "rgb(238, 246, 255)";
         mainTitle.style.color = "rgb(238, 246, 255)";
